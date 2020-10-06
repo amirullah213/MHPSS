6c2f9a43b8b786df13784bdaa05ddb3d665eb6a9
Arshad Ali <aali@ozoqo.com>
2020-10-06 10:40:01 +0500

    reception new register

---
+++ src/app/reception/new-register/new-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ncri-new-register',
+  templateUrl: './new-register.component.html',
+  styleUrls: ['./new-register.component.scss']
+})
+export class NewRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/reception/reception.module.ts
+++ src/app/reception/reception.module.ts
@@ -6,11 +6,12 @@ import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReceptionComponent } from './reception/reception.component';
 import { SharedModule } from '../core';
+import { NewRegisterComponent } from './new-register/new-register.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, ReceptionComponent],
+  declarations: [HomeComponent, ReceptionComponent, NewRegisterComponent],
   imports: [
     CommonModule,
     ReceptionRoutingModule, FormsModule,ReactiveFormsModule,SharedModule
