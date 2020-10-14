b6e4b6e47a34d4b71f64e33d00dd0462908e32df
Amir ullah <70256195+amirullah213@users.noreply.github.com>
2020-10-14 10:47:38 +0500

    ot

--- src/app/operation/operation.module.ts
+++ src/app/operation/operation.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OperationRoutingModule } from './operation-routing.module';
-import { OperationComponent } from './operation/operation.component';
 import { HomeComponent } from './home/home.component';
 import { OperationSideBarComponent } from './operation-side-bar/operation-side-bar.component';
 import { OtComponent } from './ot/ot.component';
 
 
 @NgModule({
-  declarations: [OperationComponent, HomeComponent, OperationSideBarComponent, OtComponent],
+  declarations: [HomeComponent, OperationSideBarComponent, OtComponent],
   imports: [
     CommonModule,
     OperationRoutingModule
