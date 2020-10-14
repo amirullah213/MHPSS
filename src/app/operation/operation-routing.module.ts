1aead8910fe17c32fbf89b2fa97101d6392ca2d2
Amir ullah <70256195+amirullah213@users.noreply.github.com>
2020-10-14 10:48:08 +0500

    ot

--- src/app/operation/operation-routing.module.ts
+++ src/app/operation/operation-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OtComponent } from './ot/ot.component';
-import { OperationComponent } from './operation/operation.component';
 
 
 const routes: Routes = [{ path: '', component: OtComponent }];
