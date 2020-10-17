e16346692ffdc47f0d291e67c0dc6a3ae8136805
aali61231 <70698507+aali61231@users.noreply.github.com>
2020-10-17 18:22:57 +0500

    admin commit

--- src/app/admin/diagnostic-list/diagnostic-list.component.ts
+++ src/app/admin/diagnostic-list/diagnostic-list.component.ts
@@ -11,8 +11,9 @@ import { AdminServiceService } from '../services/admin-service.service';
   styleUrls: ['./diagnostic-list.component.scss']
 })
 export class DiagnosticListComponent implements OnInit {
+  showTable:boolean=false;
   diagnosis: any = [];
-  selected: string;
+  selected: any='';
   userOb: any = {};
   EuserOb: any = {};
   dtOptions: any = {};
@@ -20,6 +21,7 @@ export class DiagnosticListComponent implements OnInit {
   model2: any = {};
   modalRef: BsModalRef;
   model5: any = {};
+  model10: any = {};
   allDiag: any = [];
   loader_eqp: boolean = false;
   errormsg: string;
@@ -27,6 +29,10 @@ export class DiagnosticListComponent implements OnInit {
   userID: number;
   dtElement: DataTableDirective;
   dtInstance: DataTables.Api;
+  selectedData:any={};
+  model9:any={};
+  Diagnosticname:any;
+  ambID:number;
   // We use this trigger because fetching the list of persons can be quite long,
   // thus we ensure the data is fetched before rendering
   //dtTrigger: Subject = new Subject();
@@ -50,18 +56,7 @@ export class DiagnosticListComponent implements OnInit {
     );
   }
   
-  rerender(): void {
-    try {
-      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
-        // Destroy the table first
-        dtInstance.destroy();
-        // Call the dtTrigger to rerender again
-        this.dtTrigger.next();
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  
   //get all diagnostic list
   getAllDiagnostics() {
     this.loader_eqp = true;
@@ -73,16 +68,17 @@ export class DiagnosticListComponent implements OnInit {
         (response: any) => {
           if (response.status === 0) {
             this.diagnosis = response.data;
-            console.log('this.diagnosis==', this.diagnosis)
-
-
+            console.log('this.diagnosis==', this.diagnosis);
             this.loader_eqp = false;
+            
+            
           }
 
           if (response.status === 1) {
-            this.errormsg = response.errors;
+            this.errormsg = response.error;
             this.loader_eqp = false;
             console.log('error=', this.errormsg);
+            alert('Error in Service! Refresh the page')
             //this._loginserviceService.logout();
           }
         },
@@ -94,4 +90,77 @@ export class DiagnosticListComponent implements OnInit {
   showData(td) {
     console.log('show data==', td)
   }
+  onSelectDiagnos(sdata){
+    this.showTable=true;
+    this.selectedData=sdata.item;
+    console.log('selected data==',this.selectedData);
+  }
+
+  //add diagnosis
+  addNewDiagnosis(dNam) {
+    this.loader_eqp = true;
+
+    this.model9.name = dNam;
+    console.log('this.selected.length==', this.model9)
+   
+      this.adminService.addDiagnosis(this.model9).subscribe(
+        (response: any) => {
+          if (response.status === 0) {
+            
+            this.modalRef.hide()
+            this.loader_eqp = false;
+            this.showTable=false;
+            alert("Diagnosis Added Successfuly");
+          }
+
+          if (response.status === 1) {
+            this.errormsg = response.error;
+            this.loader_eqp = false;
+            console.log('error=', this.errormsg);
+            alert('Error in Service! Refresh the page')
+            //this._loginserviceService.logout();
+          }
+        },
+        (error) => { }
+      );
+    
+  }
+  //get all diagnostic list
+  openModdelete(Ambdelete: TemplateRef<any>,dataOb) {
+    this.ambID = dataOb;
+    this.modalRef = this.modalService.show(Ambdelete, Object.assign({}, { class: 'modal-sm' }));
+    console.log('med id====',this.ambID);
+  }
+  //add diagnosis
+  deleteDiagnosis() {
+    this.loader_eqp = true;
+
+    this.model10.id = this.ambID;
+    console.log('this.selected.length==', this.model10)
+   
+      this.adminService.deletDiagnosis(this.model10).subscribe(
+        (response: any) => {
+          if (response.status === 0) {
+            
+            this.modalRef.hide();
+            this.showTable=false;
+            this.loader_eqp = false;
+            this.selectedData={};
+            this.selected='';
+            alert("Diagnosis deleted Successfuly");
+          }
+
+          if (response.status === 1) {
+            this.errormsg = response.error;
+            this.loader_eqp = false;
+            console.log('error=', this.errormsg);
+            alert('Error in Service! Refresh the page')
+            //this._loginserviceService.logout();
+          }
+        },
+        (error) => { }
+      );
+    
+  }
+  //get all diagnostic list
 }
