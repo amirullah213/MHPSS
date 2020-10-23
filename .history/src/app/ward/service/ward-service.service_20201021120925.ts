60114250533b8a75242b16d282460d3f50dc2485
Arshad Ali <aali@ozoqo.com>
2020-10-23 14:11:24 +0500

    new commit

---
+++ .history/src/app/ward/service/ward-service.service_20201021120925.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APP_CONFIG } from '../../core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WardServiceService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+  getWardPats(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getpatients';
+    return this.http.post(url, obj);
+  }
+  setbeds(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'setbedspms';
+    return this.http.post(url, obj);
+  }
+  admitPat(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'admitpatient';
+    return this.http.post(url, obj);
+  }
+  dischargeCard(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getdischargecard ';
+    return this.http.post(url, obj);
+  }
+  getindoordetails(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getindoordetail ';
+    return this.http.post(url, obj);
+  }
+   
+}
