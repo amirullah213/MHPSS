897435f8ba45d4e436782d1ef9c06752207f0aa4
Arshad Ali <aali@ozoqo.com>
2020-11-04 14:58:29 +0500

    today push

---
+++ .history/src/app/rad-radiology/diagnosis-pending-rad/diagnosis-pending-rad.component_20201009145610.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { Subject } from 'rxjs';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+@Component({
+  selector: 'ncri-diagnosis-pending-rad',
+  templateUrl: './diagnosis-pending-rad.component.html',
+  styleUrls: ['./diagnosis-pending-rad.component.scss']
+})
+export class DiagnosisPendingRadComponent implements OnInit {
+  isCollapsed = true;
+
+  currentDate = new Date();
+  form = new FormGroup({
+    dateYMD: new FormControl(new Date()),
+    dateFull: new FormControl(new Date()),
+    dateMDY: new FormControl(new Date()),
+    dateRange: new FormControl([
+      new Date(),
+      new Date(this.currentDate.setDate(this.currentDate.getDate() + 7))
+    ])
+  });
+
+  model: any = {};
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService, private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+  }
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'gray modal-lg' })
+    );
+  }
+
+}
