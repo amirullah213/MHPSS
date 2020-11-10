11f8480b5886af43d5091dcc0a3992c05c1876f2
DESKTOP-0C7SEUT\Nain Khan <zulqarnain@ozoqo.com>
2020-11-10 16:41:47 +0500

    chnages

---
+++ src/app/operation/operation.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { APP_CONFIG } from '../core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OTService {
+ 
+
+  constructor(private http: HttpClient, private router: Router,) {}
+
+
+
+  addpatientvital(obj: any):Observable<any> {
+    let url = APP_CONFIG.apiBaseUrl + 'addpatientvital';
+    return this.http.post(url, obj);
+  }
+  getallpatientvital(obj: any):Observable<any> {
+    let url = APP_CONFIG.apiBaseUrl + 'getallpatientvital';
+    return this.http.post(url, obj);
+  }
+
+  getpatients(obj: any):Observable<any> {
+    let url = APP_CONFIG.apiBaseUrl + 'getpatients';
+    return this.http.post(url, obj);
+  }
+
+}
