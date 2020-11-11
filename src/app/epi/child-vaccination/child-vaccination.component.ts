bd52c1cbef511210fb62d8c475734e0f16e4ea38
DESKTOP-0C7SEUT\Nain Khan <zulqarnain@ozoqo.com>
2020-11-11 11:27:23 +0500

    chng

--- src/app/epi/child-vaccination/child-vaccination.component.ts
+++ src/app/epi/child-vaccination/child-vaccination.component.ts
@@ -11,23 +11,61 @@ export class ChildVaccinationComponent implements OnInit {
   userLoader: boolean;
   paramData: any={};
   detail: any={};
+  epiData: any=[{"sr": 1,
+  "when": "At Birth",
+  "age": "At Birth",
+  "vaccines": "BCG,OPV-0,Hep-B"}
+,
+{"sr":2 ,
+    "when": "At Birth",
+    "age": "At Birth",
+    "vaccines": "BCG,OPV-0,Hep-B"},
+    {"sr": 3,
+    "when": "At Birth",
+    "age": "At Birth",
+    "vaccines": "BCG,OPV-0,Hep-B"}
+  ,
+  {"sr": 4,
+  "when": "At Birth",
+  "age": "At Birth",
+  "vaccines": "BCG,OPV-0,Hep-B"},
+  {"sr": 5,
+  "when": "At Birth",
+  "age": "At Birth",
+  "vaccines": "BCG,OPV-0,Hep-B"},
+  {"sr": 6,
+  "when": "At Birth",
+  "age": "At Birth",
+  "vaccines": "BCG,OPV-0,Hep-B"}];
+  epiRes: any=[];
 
   constructor(  private eService: epiService,
     private router: Router) { }
 
   ngOnInit(): void {
-    this.patInfo =localStorage.getItem("patData");
-    this.detail =localStorage.getItem("details");
+    this.patInfo =JSON.parse(localStorage.getItem("patData"));
+    this.detail =JSON.parse(localStorage.getItem("details"));
 
     this.getchildrenepischedule();
   }
   getchildrenepischedule(){
 debugger
     this.userLoader=true;
-    this.paramData= {"tokenID":this.detail.token,"hospitalID":this.detail.hospitalID,"patientID":this.patInfo.patientID,"departmentID":this.patInfo.departmentID}
+    this.paramData= {"tokenID":this.patInfo.token,"hospitalID":this.detail.hospitalID,"patientID":this.patInfo.patientID,"departmentID":this.patInfo.departmentID}
 
     this.eService.getchildrenepischedule(this.paramData).subscribe((response: any) => {
       if (response.status == 0) {
+        this.epiRes = response.data
+
+        if(this.epiRes.length==0)
+        {
+
+
+
+        }
+        else{
+
+        }
                 this.userLoader = false;
       } else {
         this.userLoader = false;
