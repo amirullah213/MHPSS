872a0f35137271d886d22b5c34ae857854b209b6
DESKTOP-0C7SEUT\Nain Khan <zulqarnain@ozoqo.com>
2020-11-11 16:52:47 +0500

    chnages

--- src/app/epi/epi.serivce.ts
+++ src/app/epi/epi.serivce.ts
@@ -13,10 +13,19 @@ export class epiService {
   constructor(private http: HttpClient, private router: Router,) {}
 
 
-   getchildrenepischedule(obj: any):Observable<any> {
+  getchildrenepischedule(obj: any):Observable<any> {
     let url = APP_CONFIG.apiBaseUrl + 'getchildrenepischedule';
     return this.http.post(url, obj);
   }
  
-
+  addchildrenepischedule(obj: any):Observable<any> {
+    let url = APP_CONFIG.apiBaseUrl + 'addchildrenepischedule';
+    return this.http.post(url, obj);
+  }
+  updatechildrenepischedule(obj: any):Observable<any> {
+    let url = APP_CONFIG.apiBaseUrl + 'updatechildrenepischedule';
+    return this.http.post(url, obj);
+  }
+ 
+  
 }
