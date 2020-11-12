79142de060da0930ab204dd3d8eea9be52c1a8e3
Arshad Ali <aali@ozoqo.com>
2020-11-12 18:16:14 +0500

    615 night

---
+++ .history/src/app/operation/operation.module_20201112164953.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { DataTablesModule } from 'angular-datatables';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { OperationRoutingModule } from './operation-routing.module';
+import { HomeComponent } from './home/home.component';
+import { OperationSideBarComponent } from './operation-side-bar/operation-side-bar.component';
+import { OtComponent } from './ot/ot.component';
+import { PendingPatientsComponent } from './pending-patients/pending-patients.component';
+import { SeenPatientsComponent } from './seen-patients/seen-patients.component';
+import { TypeaheadModule } from 'ngx-bootstrap';
+
+
+@NgModule({
+  declarations: [HomeComponent, OperationSideBarComponent, OtComponent, PendingPatientsComponent, SeenPatientsComponent],
+  imports: [
+    OperationRoutingModule, ReactiveFormsModule, CommonModule, SharedModule, DataTablesModule, ModalModule.forRoot(), FormsModule, BsDatepickerModule.forRoot(), CollapseModule.forRoot(),TypeaheadModule.forRoot(),
+   
+  ]
+})
+export class OperationModule { }
---
+++ .history/src/app/operation/services/ot-service.service_20201112152912.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { APP_CONFIG } from '../../core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OtServiceService {
+  constructor(private http: HttpClient, private router: Router) { }
+  getPatList(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getpatients   ';
+    return this.http.post(url, obj);
+  } 
+  getpendingTests(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getpatientpendingtest';
+    return this.http.post(url, obj);
+  } 
+  updateTests(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'updattest ';
+    return this.http.post(url, obj);
+  } 
+  getOperationTheatre(obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getoperationtheatre ';
+    return this.http.post(url, obj);
+  } 
+
+  updateOperationTheatre (obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'updateoperationtheatre  ';
+    return this.http.post(url, obj);
+  }
+  getIndoorList (obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getindoorlist  ';
+    return this.http.post(url, obj);
+  }
+  getInvestigations (obj: any) {
+    let url = APP_CONFIG.apiBaseUrl + 'getinvestigations  ';
+    return this.http.post(url, obj);
+  }
+  
+   
+}
