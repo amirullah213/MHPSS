cca67bb22fac144c83ba32f2f04cee5f8693123e
Arshad Ali <aali@ozoqo.com>
2020-11-18 15:59:39 +0500

    last 400

--- src/app/print-module/print-module.module.ts
+++ src/app/print-module/print-module.module.ts
@@ -5,10 +5,12 @@ import { PrintModuleRoutingModule } from './print-module-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PrintComponent } from './print/print.component';
 import { PrintSideBarComponent } from './print-side-bar/print-side-bar.component';
+import { RadiologyComponent } from './radiology/radiology.component';
+import { PathologyComponent } from './pathology/pathology.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, PrintComponent, PrintSideBarComponent],
+  declarations: [HomeComponent, PrintComponent, PrintSideBarComponent, RadiologyComponent, PathologyComponent],
   imports: [
     CommonModule,
     PrintModuleRoutingModule
