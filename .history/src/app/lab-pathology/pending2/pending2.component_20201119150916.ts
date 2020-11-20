afe532333fefe8ab22c439635c9a42b50c1010ab
Arshad Ali <aali@ozoqo.com>
2020-11-20 10:58:35 +0500

    last test 1058

---
+++ .history/src/app/lab-pathology/pending2/pending2.component_20201119150916.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ncri-pending2',
+  templateUrl: './pending2.component.html',
+  styleUrls: ['./pending2.component.scss']
+})
+export class Pending2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
