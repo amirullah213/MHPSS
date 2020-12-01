d92a48c55e2bdc911ed581de18f7be6dd128ba21
aali61231 <70698507+aali61231@users.noreply.github.com>
2020-12-01 16:44:43 +0500

    consumeable commit

--- src/app/pharmacy/dispense-stock/dispense-stock.component.ts
+++ src/app/pharmacy/dispense-stock/dispense-stock.component.ts
@@ -39,6 +39,7 @@ export class DispenseStockComponent implements OnInit {
   pharmacyid:any;
   modal1:any={};
   stckArr:any=[];
+   medStr:any;
 
 
   ngOnInit(): void {
@@ -65,8 +66,12 @@ export class DispenseStockComponent implements OnInit {
  this.pharmacySer.getPharmacyStock(this.modal1).subscribe(
     (response: any) => {
       if (response.status === 0) {
-        this.pharmacyData = response.data;
-      console.log('this.pharmacy pats==',this.pharmacyData)
+        response.data.forEach(v => {
+          this.medStr = v.itemName + ", "+ v.unit+ " "+ v.type;                                      
+          this.pharmacyData.push({"itemName":this.medStr,v});
+          // console.log('gettreatmetData==',this.gettreatmetData)
+        });
+      console.log('this.pharmacy pats==',this.pharmacyData);
         this.loader_eqp = false;
       }
   if (response.status === 1) {
@@ -84,10 +89,12 @@ export class DispenseStockComponent implements OnInit {
 onSelectMedics(ob)
 {
   console.log('medics data selected==',ob.item);
-  this.grnobj.unit=ob.item.unit;
-  this.grnobj.type=ob.item.type;
-  this.grnobj.itemID=ob.item.itemID;
-  this.stckArr=ob.item.stock;
+  this.grnobj.unit=ob.item.v.unit;
+  this.grnobj.type=ob.item.v.type;
+  this.grnobj.itemID=ob.item.v.itemID;
+  this.stckArr=ob.item.v.stock;
+  
+  
   
 }
 addToMedicArr(medArr){
