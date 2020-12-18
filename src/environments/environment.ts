87a236732dddd5cf47caed4ec5416e36acb5d1e4
arshad <aali@ozoqo.com>
2020-12-18 13:26:13 +0500

    changes..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
  // apiBaseUrl: 'https://tma4nljbmd.execute-api.us-west-1.amazonaws.com/dev/'
  
- //apiBaseUrl: 'http://3.225.249.153:3000/',
- 
  apiBaseUrl: 'http://3.225.249.153:3000/',
+ 
+ //apiBaseUrl: 'http://192.168.43.76:3000/',
 
 
  apiBaseUrlOnline: 'https://yoa6yqfdfg.execute-api.us-east-1.amazonaws.com/merf_dhis/'
